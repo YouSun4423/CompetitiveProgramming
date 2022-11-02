fa4c78cfa5208b681391fcda745ed629347560b0
矢口悠月 <youuu.sunnn@gmail.com>
2022-11-02 15:37:01 +0900

    二分探索のアルゴリズムを改善

--- Algorithms/BinarySearch.py
+++ Algorithms/BinarySearch.py
@@ -1,25 +1,23 @@
-def binary_search(N, A, k):
-    ans = -1
+def binary_search(N, A, x):
     l = 0
     r = N - 1
     while r-l+1>=1:
         m = (r+l) // 2
-        if A[m] == k:
-            ans = m
-            print(ans)
+        if A[m] == x:
+            print(m)
             return m
-        if A[m] < k:
+        elif A[m] < x:
             l = m + 1
-        if A[m] > k:
-            ans = m
+        elif A[m] > x:
             r = m - 1
-    print(ans)
     return -1
 
-
-N, k = map(int, input().split())
+#配列の長さ
+n=int(input())
 #配列A
 a=list(map(int,input().split()))
+#見つけたい値
+x=int(input())
 
 #関数を実行
-binary_search(N,a,k)
+binary_search(n,a,x)
